40ee9cb9871c740cc4cabd6756f09c36084e610e
lmarz <leon.marz@kabelmail.de>
2020-03-19 11:57:43 +0100

    Add file upload functionality

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const PORT = 4242;
 const express = require("express");
 const app = express();
 const path = require("path");
+const body_parser = require("body-parser");
 
 /* CONFIGURE EXPRESS */
 app.use(express.static(path.join(__dirname, "/../client"))); /* Client-requests */
 app.use("/cds", express.static(path.join(__dirname, "/../cds"))); /* Content-requests */
+app.use(body_parser.text());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "../client/index.html");
@@ -14,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.send("Test.");
+});
+
+app.post("/upload", (req, res) => {
+    console.log(JSON.parse(req.body));
+    res.end();
 });
 
 app.listen(PORT, () => {
