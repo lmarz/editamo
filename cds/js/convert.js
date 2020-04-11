778b07c2b1a46db4b4dac1ddd3b4baf1c5e0e56e
lmarz <leon.marz@kabelmail.de>
2020-04-11 11:44:42 +0200

    Add GLTF2AMO functionality

--- cds/js/convert.js
+++ cds/js/convert.js
@@ -109,6 +109,47 @@ function loadForAMO(gltfModel) {
     weightBuffer.start = gltfModel.bufferViews[weightBuffer.bufferView].byteOffset;
     weightBuffer.stop = weightBuffer.start + gltfModel.bufferViews[weightBuffer.bufferView].byteLength;
 
+    let animation = {};
+    animation.name = gltfModel.animations[0].name;
+    animation.joints = [];
+    for(let i = 0; i < gltfModel.skins[0].joints.length; i++) {
+        animation.joints[i] = {};
+        animation.joints[i].index = gltfModel.skins[0].joints[i];
+        animation.joints[i].name = gltfModel.nodes[animation.joints[i].index].name;
+        animation.joints[i].children = gltfModel.nodes[animation.joints[i].index].children;
+        if(animation.joints[i].children != undefined) {
+            for(let j = 0; j < animation.joints[i].children.length; j++) {
+                animation.joints[animation.joints[i].children[j]].parent = i; 
+            }
+        }
+    }
+    animation.samplers = [];
+    for(let i = 0; i < gltfModel.animations[0].samplers.length; i++) {
+        animation.samplers[i] = {};
+        animation.samplers[i].timeBuffer = {};
+        animation.samplers[i].timeBuffer.accessor = gltfModel.animations[0].samplers[i].input;
+        animation.samplers[i].timeBuffer.bufferView = gltfModel.accessors[animation.samplers[i].timeBuffer.accessor].bufferView;
+        animation.samplers[i].timeBuffer.count = gltfModel.accessors[animation.samplers[i].timeBuffer.accessor].count;
+        animation.samplers[i].timeBuffer.buffer = gltfModel.bufferViews[animation.samplers[i].timeBuffer.bufferView].buffer;
+        animation.samplers[i].timeBuffer.start = gltfModel.bufferViews[animation.samplers[i].timeBuffer.bufferView].byteOffset;
+        animation.samplers[i].timeBuffer.stop = animation.samplers[i].timeBuffer.start + gltfModel.bufferViews[animation.samplers[i].timeBuffer.bufferView].byteLength;
+
+        animation.samplers[i].valueBuffer = {};
+        animation.samplers[i].valueBuffer.accessor = gltfModel.animations[0].samplers[i].output;
+        animation.samplers[i].valueBuffer.bufferView = gltfModel.accessors[animation.samplers[i].valueBuffer.accessor].bufferView;
+        animation.samplers[i].valueBuffer.count = gltfModel.accessors[animation.samplers[i].valueBuffer.accessor].count;
+        animation.samplers[i].valueBuffer.buffer = gltfModel.bufferViews[animation.samplers[i].valueBuffer.bufferView].buffer;
+        animation.samplers[i].valueBuffer.start = gltfModel.bufferViews[animation.samplers[i].valueBuffer.bufferView].byteOffset;
+        animation.samplers[i].valueBuffer.stop = animation.samplers[i].valueBuffer.start + gltfModel.bufferViews[animation.samplers[i].valueBuffer.bufferView].byteLength;
+    }
+    animation.channels = [];
+    for(let i = 0; i < gltfModel.animations[0].channels.length; i++) {
+        animation.channels[i] = {};
+        animation.channels[i].joint = gltfModel.animations[0].channels[i].target.node;
+        animation.channels[i].sampler = gltfModel.animations[0].channels[i].sampler;
+        animation.channels[i].path = gltfModel.animations[0].channels[i].target.path;
+    }
+    console.log(animation);
     for(let i = 0; i < gltfModel.buffers.length; i++) {
         fetch(gltfModel.buffers[i].uri).then((res) => {
             res.arrayBuffer().then((data) => {
@@ -134,7 +175,17 @@ function loadForAMO(gltfModel) {
                 if(weightBuffer.buffer == i) {
                     weightBuffer.data = new Float32Array(data.slice(weightBuffer.start, weightBuffer.stop));
                 }
-                console.log(createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer, weightBuffer, indexBuffer));
+                for(let j = 0; j < animation.samplers.length; j++) {
+                    if(animation.samplers[j].timeBuffer.buffer == i) {
+                        animation.samplers[j].timeBuffer.data = new Float32Array(data.slice(animation.samplers[j].timeBuffer.start, animation.samplers[j].timeBuffer.stop));
+                    }
+                    if(animation.samplers[j].valueBuffer.buffer == i) {
+                        if(gltfModel.accessors[animation.samplers[j].valueBuffer.accessor].componentType == 5126) {
+                            animation.samplers[j].valueBuffer.data = new Float32Array(data.slice(animation.samplers[j].valueBuffer.start, animation.samplers[j].valueBuffer.stop));
+                        }
+                    }
+                }
+                console.log(createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer, weightBuffer, indexBuffer, animation));
             });
         });
     }
@@ -163,7 +214,7 @@ function createOBJ(gltfModel, vertexBuffer, normalBuffer, uvBuffer, indexBuffer)
     return OBJ;
 }
 
-function createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer, weightBuffer, indexBuffer) {
+function createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer, weightBuffer, indexBuffer, animation) {
     let AMO = "#Generated by editamo\n";
     AMO += `ao ${gltfModel.meshes[0].name}\n\n`;
     for(let i = 0; i < vertexBuffer.count; i++) {
@@ -179,7 +230,7 @@ function createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer,
     }
     AMO += "\n";
     for(let i = 0; i < jointBuffer.count; i++) {
-        AMO += `vj ${jointBuffer.data[i*4]} ${jointBuffer.data[i*4+1]} ${jointBuffer.data[i*4+2]} ${jointBuffer.data[i*4+3]}\n`;
+        AMO += `vj ${jointBuffer.data[i*4]+1} ${jointBuffer.data[i*4+1]+1} ${jointBuffer.data[i*4+2]+1} ${jointBuffer.data[i*4+3]+1}\n`;
     }
     AMO += "\n";
     for(let i = 0; i < weightBuffer.count; i++) {
@@ -192,6 +243,25 @@ function createAMO(gltfModel, vertexBuffer, normalBuffer, uvBuffer, jointBuffer,
         AMO += `${indexBuffer.data[i*3+2]+1}/${indexBuffer.data[i*3+2]+1}/${indexBuffer.data[i*3+2]+1}/${indexBuffer.data[i*3+2]+1}/${indexBuffer.data[i*3+2]+1}\n`;
     }
     AMO += "\n";
-    AMO += `a ${gltfModel.animations[0].name}\n\n`;
+    for(let i = 0; i < animation.joints.length; i++) {
+        if(animation.joints[i].parent != undefined) {
+            AMO += `j ${animation.joints[i].name} ${animation.joints[i].parent}\n`;
+        } else {
+            AMO += `j ${animation.joints[i].name} -1\n`;
+        }
+    }
+    AMO += "\n";
+    AMO += `a ${animation.name}\n\n`;
+    for(let i = 0; i < animation.channels.length; i++) {
+        let sampler = animation.samplers[animation.channels[i].sampler];
+        for(let j = 0; j < sampler.timeBuffer.count; j++) {
+            if(animation.channels[i].path == "translation") {
+                AMO += `ap ${sampler.timeBuffer.data[j]} ${animation.channels[i].joint+1} ${sampler.valueBuffer.data[j*3]} ${sampler.valueBuffer.data[j*3+1]} ${sampler.valueBuffer.data[j*3+2]}\n`;
+            } else if(animation.channels[i].path == "rotation") {
+                AMO += `ar ${sampler.timeBuffer.data[j]} ${animation.channels[i].joint+1} ${sampler.valueBuffer.data[j*4]} ${sampler.valueBuffer.data[j*4+1]} ${sampler.valueBuffer.data[j*4+2]} ${sampler.valueBuffer.data[j*4+3]}\n`;
+            }
+        }
+    }
+    AMO += "\n";
     return AMO;
 }
